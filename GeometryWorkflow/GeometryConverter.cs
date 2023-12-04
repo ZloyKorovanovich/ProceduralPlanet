528101a7c22089f2e5cb0c032de974c56e11ff76
Mitrofan <118289864+ZloyKorovanovich@users.noreply.github.com>
2023-12-04 18:05:19 +0300

    Add files via upload

---
+++ GeometryWorkflow/GeometryConverter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace GeometryWorkflow
+{
+    public static class GeometryConverter
+    {
+        public static void ConvertDataToMesh(GeometryData geometryData, out Mesh mesh)
+        {
+            mesh = new Mesh();
+            mesh.SetVertices(geometryData.Vertices);
+            mesh.SetTriangles(geometryData.Triangles, 0);
+            mesh.SetUVs(0, geometryData.UVs);
+        }
+
+        public static void ConvertMeshToData(Mesh mesh, out GeometryData geometryData)
+        {
+            geometryData = new GeometryData(mesh.vertices, mesh.triangles, mesh.uv);
+        }
+    }
+}
